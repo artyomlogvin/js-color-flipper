359eec3768b5d0b4208e050deb47935d28c31d4e
rumi <43935362+artyomlogvin@users.noreply.github.com>
2024-01-25 20:24:44 +0300

    Add changeText()

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const colors = ["Red", "Green", "Blue"];
 function pickRandomColor() {
     return colors[Math.random() * colors.length];
 }
+
+function changeText(newText) {
+    const textbox = document.querySelector('.center-text');
+    textbox.textContent = newText;
+}
